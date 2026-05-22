24b3a22a1e433a73e3c6a96fc0b6ec06faed0a9e
Sergey <sergeyportnov01@gmail.com>
2026-05-23 01:51:05 +0300

    Раскоментирование решений

--- src/task_lvl2.c
+++ src/task_lvl2.c
@@ -19,40 +19,40 @@ area1234 --> Area1234
 ----------------------------------------------------------------------*/
 
 
-// int main() {
-//     char *str = NULL;
-//     size_t i = 0;
-//     char c;
+int main() {
+    char *str = NULL;
+    size_t i = 0;
+    char c;
 
-//     while (scanf("%c", &c) == 1 && c != '\n') {
-//         str = realloc(str, (i + 1) * sizeof(char));
-//         str[i] = c;
-//         i++;
-//     }
+    while (scanf("%c", &c) == 1 && c != '\n') {
+        str = realloc(str, (i + 1) * sizeof(char));
+        str[i] = c;
+        i++;
+    }
 
-//     if (str == NULL) {
-//         return 0;
-//     }
+    if (str == NULL) {
+        return 0;
+    }
 
-//     str[i] = '\0';
+    str[i] = '\0';
 
-//     if (str[0] >= 'a' && str[0] <= 'z') {
-//         str[0] = str[0] - 32;
-//     }
+    if (str[0] >= 'a' && str[0] <= 'z') {
+        str[0] = str[0] - 32;
+    }
 
-//     int j = 0;
+    int j = 0;
 
-//     while (j < i) {
-//         if (str[j] == ' ' && str[j + 1] >= 'a' && str[j + 1] <= 'z') {
-//             str[j + 1] = str[j + 1] - 32;
-//         }
-//         j++;
-//     }
+    while (j < i) {
+        if (str[j] == ' ' && str[j + 1] >= 'a' && str[j + 1] <= 'z') {
+            str[j + 1] = str[j + 1] - 32;
+        }
+        j++;
+    }
 
-//     printf("%s", str);
-//     free(str);
-//     return 0;
-// }
+    printf("%s", str);
+    free(str);
+    return 0;
+}
 
 /* ---------------------------------------------------------------------
 Task 2 - Каталог фильмов
@@ -86,73 +86,73 @@ typedef struct {
     int year;
 } Film;
 
-// void fill_film(Film *films, int n);
-// void get_director(char *str);
-// void print_res(Film *films, char *search, int n);
+void fill_film(Film *films, int n);
+void get_director(char *str);
+void print_res(Film *films, char *search, int n);
 
-// int main() {
-//     int n;
-//     if (scanf("%d", &n) != 1) {
-//         return 0;
-//     };
-//     getchar();
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) {
+        return 0;
+    };
+    getchar();
 
-//     Film films[n];
-//     fill_film(films, n);
+    Film films[n];
+    fill_film(films, n);
 
-//     char search[100];
-//     get_director(search);
+    char search[100];
+    get_director(search);
     
-//     print_res(films, search, n);
+    print_res(films, search, n);
     
-//     return 0;
-// }
+    return 0;
+}
 
-// void fill_film(Film *films, int n) {
-//     for (int i = 0; i < n; i++) {
-//         char temp[100];
-//         char c;
-//         int j = 0;
-//         while (scanf("%c", &c) == 1 && c != '\n' && c != ';') {
-//             if (j < 99) temp[j++] = c;
-//         }
-//         temp[j] = '\0';
-//         strcpy(films[i].name, temp);
+void fill_film(Film *films, int n) {
+    for (int i = 0; i < n; i++) {
+        char temp[100];
+        char c;
+        int j = 0;
+        while (scanf("%c", &c) == 1 && c != '\n' && c != ';') {
+            if (j < 99) temp[j++] = c;
+        }
+        temp[j] = '\0';
+        strcpy(films[i].name, temp);
 
-//         memset(temp, 0, sizeof(temp));
-//         j = 0;
-//         while (scanf("%c", &c) == 1 && c != '\n' && c != ';') {
-//             if (j < 99) temp[j++] = c;
-//         }
-//         temp[j] = '\0';
-//         strcpy(films[i].director, temp);
+        memset(temp, 0, sizeof(temp));
+        j = 0;
+        while (scanf("%c", &c) == 1 && c != '\n' && c != ';') {
+            if (j < 99) temp[j++] = c;
+        }
+        temp[j] = '\0';
+        strcpy(films[i].director, temp);
 
-//         scanf("%d", &films[i].year);
-//         getchar();
-//     }
-// }
+        scanf("%d", &films[i].year);
+        getchar();
+    }
+}
 
-// void get_director(char *str) {
-//     char c;
-//     int j = 0;
-//     while (scanf("%c", &c) == 1 && c != '\n') {
-//             str[j++] = c;
-//         }
-//     str[j] = '\0';
-// }
+void get_director(char *str) {
+    char c;
+    int j = 0;
+    while (scanf("%c", &c) == 1 && c != '\n') {
+            str[j++] = c;
+        }
+    str[j] = '\0';
+}
 
-// void print_res(Film *films, char *search, int n) {
-//     int found = 0;
-//     for (int i = 0; i < n; i++) {
-//         if (strcmp(films[i].director, search) == 0) {
-//             printf("%s (%d)\n", films[i].name, films[i].year);
-//             found ++;
-//         }
-//     }
-//     if (found == 0) {
-//         printf("Not found");
-//     }
-// }
+void print_res(Film *films, char *search, int n) {
+    int found = 0;
+    for (int i = 0; i < n; i++) {
+        if (strcmp(films[i].director, search) == 0) {
+            printf("%s (%d)\n", films[i].name, films[i].year);
+            found ++;
+        }
+    }
+    if (found == 0) {
+        printf("Not found");
+    }
+}
 
 // решение через fgets и strtok
 
